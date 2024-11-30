fe499dfe6cd4fca77cc8ba54efdb634aa4bd066c
Aayan Dutta <83695121+aayan-dutta@users.noreply.github.com>
2024-12-01 00:04:04 +0530

    Add a simple to-do list app
    todo list task stuff

--- api/index.py
+++ api/index.py
@@ -1,14 +1,25 @@
-from flask import Flask, request, render_template
+from flask import Flask, request, render_template, redirect, url_for
 
 app = Flask(__name__)
 
-@app.route('/')
-def home():
-    return render_template('index.html')
+tasks=[]
 
-@app.route('/about')
-def about():
-    return 'About'
+@app.route('/')
+def index():
+    return render_template('index.html', tasks=tasks)
+
+@app.route('/add', methods=['POST'])
+def add_task():
+    task = request.form.get('task')
+    if task:
+        task.append(task)
+    return redirect(url_for('index'))
+
+@app.route('/delete/<inte:task_id>', methods = ['POST'])
+def delete_task(task_id):
+    if 0 <= task_id < len(tasks):
+        tasks.pop(task_id)
+    return redirect(url_for('index'))
 
 if __name__ == '__main__':
     app.run(debug=True)
